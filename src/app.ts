6519cec7027bce0c3530c93af54f5200b70ee13c
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-05-18 18:37:32 +0600

    add start:dev and prod

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
-const port = 3000;
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
+const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  var a = 10;
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
 
   res.send(a);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
 
 export default app;
